af33ca50a314e7906064c8e4ef45a4ad8b36485b
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-26 14:16:28 -0400

    commit tree fa10f8472e15a06cb06897e1c39c88c75de80468

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, string) {
+    let res = array.filter(name =>  name.toLowerCase() === string.toLowerCase() || name.toLowerCase() === string.toLowerCase())
+    return res;
+}
+
+function fuzzyMatch(array, string) {
+    let res = array.filter(name => name.substring(0,1) === string.substring(0,1))
+    return res
+}
+
+function matchName(array, string) {
+    let res = array.filter(name => name.name === string)
+    return res
+}
